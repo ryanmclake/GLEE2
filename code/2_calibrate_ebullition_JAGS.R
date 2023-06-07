a97bbd70d737924e04a78674b217ce0d1512572a
Ryan McClure <rmcclure@CIS-RMcclure-Air.local>
2023-06-07 09:13:54 -0600

    annotate scripts to be a little mode clear for MEL and other testers

--- code/2_calibrate_ebullition_JAGS.R
+++ code/2_calibrate_ebullition_JAGS.R
@@ -1,5 +1,11 @@
+
+# Clean your environment
 rm(list=ls())
+
+#Clean your memory
 gc()
+
+# Set seed for repeatability
 set.seed(3818)
 
 # install packages needed for the analysis
@@ -13,6 +19,7 @@ pacman::p_load(tidyverse, MCMCvis, lubridate, tidybayes, readr,
 # Load in the prior Ebullition Estimates
 ebu_coefficients <- read_csv("./source_data/ebullition_coefficients.csv")
 
+# obtain the summary statistics around our ebullition priors
 summary(ebu_coefficients)
 sd(ebu_coefficients$omega)
 sd(ebu_coefficients$E20)
@@ -47,26 +54,35 @@ model {
    }
   }", file = arrhenius_ebu_model)
 
-
+# Load in the data base with the assigned error
 error_ebu <- readr::read_csv("./output/filtered_GLEE_ebullition_w_GLCP_link_and_ERROR.csv") %>%
   filter(waterbody_type == "lake" | waterbody_type == "reservoir") %>%
   filter(sd_time > 0)
 
+# specify the lake type that we will filter to calibrate
+# we will calibrate lakes and reservoirs independently
 lake_type <- c(unique(error_ebu$waterbody_type))
 
+# specify our data frames that we want out
 out_raw_coefficients <- list()
 out_agg_coefficients <- list()
 
+# for loop that loops between lakes and reservoirs
 for(g in 1:length(lake_type)){
   
+  # get either a lake or reservoir
   error_ebu_new <- error_ebu %>% 
     filter(waterbody_type == lake_type[g])
   
+  # specify our data for JAGS
   jags.data = list(Y = error_ebu_new$ch4_ebu,
-                   tau.obs = 1/(error_ebu_new$sd_time) ^ 2,
+                   #tau.obs = 1/(error_ebu_new$sd_time) ^ 2,
+                   #tau.obs = 1/(error_ebu_new$sd_space_time) ^ 2,
+                   tau.obs = 1/(error_ebu_new$sd_space) ^ 2,
                    N = nrow(error_ebu_new),
                    temp = as.numeric(error_ebu_new$temp_for_model_C))
   
+  # Set up our chains
   nchain = 3
   chain_seeds <- c(200,800,1400)
   init <- list()
@@ -79,26 +95,30 @@ for(g in 1:length(lake_type)){
   }
   
   
-  
+  # prepare the model
   j.model   <- jags.model(file = arrhenius_ebu_model,
                           data = jags.data,
                           inits = init,
                           n.chains = 3)
-  
+  # evaluate the MCMCs
   eval_ebu  <- coda.samples(model = j.model,
                             variable.names = c("sd.pro","E20","omega"),
                             n.iter = 20000, n.burnin = 2000, thin = 20)
   
+  #Plot the output of the MCMC
   plot(eval_ebu)
+  # obtain the gelman diagnostics
   print(gelman.diag(eval_ebu))
   
+  #save the raw coefficient values form the MCMC run
   coefficients_raw <- eval_ebu %>%
     spread_draws(sd.pro, E20, omega) %>%
     mutate(waterbody_type = lake_type[g])
   
+  # I do this because it is a list object before the for loop
   out_raw_coefficients[[g]] <- coefficients_raw
     
-  
+  # this aggregates the coefficeients to get the mean, SD and 95% CIs of the coefficients
   coefficients_aggregate <- coefficients_raw %>%
     summarise(mean_E20 = mean(E20),
               sd_E20 = sd(E20),
@@ -118,35 +138,42 @@ for(g in 1:length(lake_type)){
   
 }
 
-
+# output the raw and aggregated coefficients as a DF
 out_raw_coefficients = as.data.frame(do.call(rbind, out_raw_coefficients))
 out_agg_coefficients = as.data.frame(do.call(rbind, out_agg_coefficients))
 
-
+# generate a posterior prediction function
 arrhenius_ebu_posterior_prediction <- function(E20, omega, temp, Q){
   est = (E20 * omega ^ (temp-20)) + rnorm(1000, 0, sd = Q)
   return(est)
 }
 
+# build to empty data frames to fill in for the model validation.
 validate_models <- list()
 validation <- list()
 
+# another loop that goes between Lakes and Reservoirs 
 for(h in 1:length(lake_type)){
 
+  # randomly sample 1000 coefficient values
 parms <- out_raw_coefficients %>%
   filter(waterbody_type == lake_type[h]) %>%
   sample_n(., 1000, replace=TRUE)
 
+  # pull the temperature data from the original DF 
 temp_data <- error_ebu %>% 
   filter(waterbody_type == lake_type[h]) %>%
   select(temp_for_model_C)
 
+  # concatinate the temp data
 temp_data <- c(temp_data$temp_for_model_C)
 
+  # pull the original observed data to compare against the predictions
 ebu_data <- error_ebu %>% 
   filter(waterbody_type == lake_type[h]) %>%
   select(ch4_ebu, temp_for_model_C, waterbody_type)
 
+  # build a loop that goes through all 1000 coefficient iterations I randomly pulled
 predict_output <- list()
 
 for(s in 1:length(temp_data)){
@@ -158,21 +185,28 @@ for(s in 1:length(temp_data)){
   predict_output[[s]] <- prediction
 }
 
+# output the predictions as a DF
 prediction_output = as.data.frame(do.call(rbind, predict_output))
 
+# aggregate the prediction output and summarize the predictions by mean, SD , and var
 prediction_output_long <- prediction_output %>% t(.) %>% reshape2::melt(.) %>%
   group_by(Var2) %>%
   summarize(mean = mean(value),
             sd = sd(value),
             var = var(value))
 
+# see how well the model does against the obervations
 validate <- cbind(prediction_output_long, ebu_data)
 
 validation[[h]] <- validate
 
+# generate an old school plot of the model output and the observations
+# the plot is basically plotting the equation 
+# where temp is on the X and ebullition observed and predcited is on the Y
 plot(validate$temp_for_model_C, validate$ch4_ebu, col = "blue4", pch = 16)
 points(validate$temp_for_model_C, validate$mean, col = "black", pch = 21, bg = "red")
 
+# determined the NSE and RMSE of the emissions in lakes and reservoirs
 NSE_RMSE <- cbind(prediction_output_long, ebu_data) %>%
   summarise(NSE = NSE(mean, ch4_ebu),
             rmse = rmse(mean, ch4_ebu))
@@ -181,5 +215,8 @@ validate_models[[h]] <- NSE_RMSE
 
 }
 
+# Get the model evaluation (NSE & RMSE) output
 validate_models = as.data.frame(do.call(rbind, validate_models))
 validation = as.data.frame(do.call(rbind, validation))
+
+
--- code/3_callibrate_diffusion_JAGS.R
+++ code/3_callibrate_diffusion_JAGS.R
@@ -1,10 +1,35 @@
+# Clean your environment
+rm(list=ls())
 
-diff_coefficients <- read_csv("./data/diffusion_coefficients.csv")
+#Clean your memory
+gc()
+
+# Set seed for repeatability
+set.seed(3818)
+
+# install packages needed for the analysis
+
+if (!"pacman" %in% installed.packages()) install.packages("pacman")
+pacman::p_load(tidyverse, MCMCvis, lubridate, tidybayes, readr,
+               ncdf4, reshape2, zoo, patchwork, hydroGOF, viridis,
+               imputeTS, devtools, scales, forecast, coda, rjags, 
+               R2jags, gridExtra, maps, hexbin, rnaturalearth, sf)
+
+
+#### NOTE ######
+# EVERYTHING BELOW IS THE SAME AS THE 2_calibrate_ebullition_JAGS.R script except the 
+# model structure in the JAGS model. Consequently, I have not annotated it as well because 
+# I honestly did not want to retype everything twice. 
+
+# Load in the prior diffusion Estimates
+diff_coefficients <- read_csv("./source_data/diffusion_coefficients.csv")
+
 summary(diff_coefficients)
-sd(diff_coefficients$B)
 sd(diff_coefficients$a)
+sd(diff_coefficients$B)
 
-ahrennius_diff_model = ("arhennius_diff_model.txt")
+# Build the JAGS MODEL
+arrhenius_diff_model = ("./output/arrhenius_diff_model.txt")
 jagsscript = cat("
 model {  
    
@@ -31,191 +56,143 @@ model {
       Y[i] ~ dnorm(X[i], tau.obs[i]) # Observation variation
             #end of data model=========================================
    }
-  }", file = ahrennius_diff_model)
+  }", file = arrhenius_diff_model)
 
 
-################################
+error_diff <- readr::read_csv("./output/filtered_GLEE_diffusion_w_GLCP_link_and_ERROR.csv") %>%
+  filter(waterbody_type == "lake" | waterbody_type == "reservoir") %>%
+  filter(sd_time > 0)
 
-### MONTHLY TIMESTEP ALL WB ###
-diff_base_temp <- base %>% select(lat, lon, ch4_diff, temp_for_model_K, waterbody_id, year, month, tot_sampling_events) %>%
-  na.omit(.) %>%
-  mutate(temp_for_model_C = temp_for_model_K-273.15) %>%
-  mutate(diff_sd = ifelse(tot_sampling_events < 5,93.6, NA),
-         diff_sd = ifelse(tot_sampling_events >= 5,25.3, diff_sd),
-         diff_sd = ifelse(tot_sampling_events > 10,2.83, diff_sd),
-         diff_sd = ifelse(tot_sampling_events > 15,6.78, diff_sd),
-         diff_sd = ifelse(tot_sampling_events > 20,0.819, diff_sd),
-         diff_sd = ifelse(tot_sampling_events > 25,0.902, diff_sd),
-         diff_sd = ifelse(tot_sampling_events > 30,0.973, diff_sd),
-         diff_sd = ifelse(tot_sampling_events > 40,0.0661, diff_sd),
-         diff_sd = ifelse(tot_sampling_events > 50,0.00665, diff_sd))
+lake_type <- c(unique(error_diff$waterbody_type))
 
+out_raw_coefficients <- list()
+out_agg_coefficients <- list()
 
-jags.data = list(Y = diff_base_temp$ch4_diff,
-                 tau.obs = 1/((diff_base_temp$diff_sd)) ^ 2,
-                 N = nrow(diff_base_temp),
-                 temp = as.numeric(diff_base_temp$temp_for_model_C))
-
-nchain = 3
-chain_seeds <- c(200,800,1400)
-init <- list()
-for(i in 1:nchain){
-  init[[i]] <- list(sd.pro = runif(1, 1, 3),
-                    A = runif(1, 0.24,1),
-                    a = runif(1, 0.023,1),
-                    .RNG.name = "base::Wichmann-Hill",
-                    .RNG.seed = chain_seeds[i])
+for(g in 1:length(lake_type)){
+  
+  error_diff_new <- error_diff %>% 
+    filter(waterbody_type == lake_type[g])
+  
+  jags.data = list(Y = error_diff_new$ch4_diff,
+                   #tau.obs = 1/(error_diff_new$sd_time) ^ 2,
+                   #tau.obs = 1/(error_diff_new$sd_space_time) ^ 2,
+                   tau.obs = 1/(error_diff_new$sd_space) ^ 2,
+                   N = nrow(error_diff_new),
+                   temp = as.numeric(error_diff_new$temp_for_model_C))
+  
+  nchain = 3
+  chain_seeds <- c(200,800,1400)
+  init <- list()
+  for(i in 1:nchain){
+    init[[i]] <- list(sd.pro = runif(1, 1, 3),
+                      A = runif(1, 0.07350, 2),
+                      a = runif(1, 0.12683, 2),
+                      .RNG.name = "base::Wichmann-Hill",
+                      .RNG.seed = chain_seeds[i])
+  }
+  
+  
+  
+  j.model   <- jags.model(file = arrhenius_diff_model,
+                          data = jags.data,
+                          inits = init,
+                          n.chains = 3)
+  
+  eval_diff  <- coda.samples(model = j.model,
+                            variable.names = c("sd.pro","A","a"),
+                            n.iter = 20000, n.burnin = 2000, thin = 20)
+  
+  plot(eval_diff)
+  print(gelman.diag(eval_diff))
+  
+  coefficients_raw <- eval_diff %>%
+    spread_draws(sd.pro, A, a) %>%
+    mutate(waterbody_type = lake_type[g])
+  
+  out_raw_coefficients[[g]] <- coefficients_raw
+  
+  
+  coefficients_aggregate <- coefficients_raw %>%
+    summarise(mean_A = mean(A),
+              sd_A = sd(A),
+              upper_95_A = quantile(A, 0.95, na.rm = T),
+              lower_95_A = quantile(A, 0.05, na.rm = T),
+              mean_a = mean(a),
+              sd_a = sd(a),
+              upper_95_a = quantile(a, 0.95, na.rm = T),
+              lower_95_a = quantile(a, 0.05, na.rm = T),
+              mean_process = mean(sd.pro),
+              sd_process = sd(sd.pro),
+              upper_95_process = quantile(sd.pro, 0.95, na.rm = T),
+              lower_95_process = quantile(sd.pro, 0.05, na.rm = T)) %>%
+    mutate(waterbody_type = lake_type[g])
+  
+  out_agg_coefficients[[g]] <- coefficients_aggregate
+  
 }
 
-j.model   <- jags.model(file = ahrennius_diff_model,
-                        data = jags.data,
-                        inits = init,
-                        n.chains = 3)
 
-eval_diff  <- coda.samples(model = j.model,
-                          variable.names = c("sd.pro","A", "a"),
-                          n.iter = 20000, n.burnin = 2000, thin = 20)
-
-plot(eval_diff)
+out_raw_coefficients = as.data.frame(do.call(rbind, out_raw_coefficients))
+out_agg_coefficients = as.data.frame(do.call(rbind, out_agg_coefficients))
 
 
-all_coefficients <- eval_diff %>%
-  spread_draws(sd.pro, A, a) %>%
-  filter(.chain == 1)
-
-all_coefficients_DF <- all_coefficients %>%
-  summarise(mean_E20 = mean(A),
-            sd_E20 = sd(A),
-            upper_95_E20 = quantile(A, 0.95, na.rm = T),
-            lower_95_E20 = quantile(A, 0.05, na.rm = T),
-            mean_omega = mean(a),
-            sd_omega = sd(a),
-            upper_95_omega = quantile(a, 0.95, na.rm = T),
-            lower_95_omega = quantile(a, 0.05, na.rm = T),
-            mean_process = mean(sd.pro),
-            sd_process = sd(sd.pro),
-            upper_95_process = quantile(sd.pro, 0.95, na.rm = T),
-            lower_95_process = quantile(sd.pro, 0.05, na.rm = T))
-
-
-### MONTHLY TIMESTEP JUST LAKES ###
-ebu_diff_temp <- base %>% select(waterbody_type, lat, lon, ch4_diff, temp_for_model_K, waterbody_id, year, month, tot_sampling_events) %>%
-  na.omit(.) %>%
-  filter(waterbody_type == "lake") %>%
-  mutate(temp_for_model_C = temp_for_model_K-273.15) %>%
-  mutate(diff_sd = ifelse(tot_sampling_events < 5,93.6, NA),
-         diff_sd = ifelse(tot_sampling_events >= 5,25.3, diff_sd),
-         diff_sd = ifelse(tot_sampling_events > 10,2.83, diff_sd),
-         diff_sd = ifelse(tot_sampling_events > 15,6.78, diff_sd),
-         diff_sd = ifelse(tot_sampling_events > 20,0.819, diff_sd),
-         diff_sd = ifelse(tot_sampling_events > 25,0.902, diff_sd),
-         diff_sd = ifelse(tot_sampling_events > 30,0.973, diff_sd),
-         diff_sd = ifelse(tot_sampling_events > 40,0.0661, diff_sd),
-         diff_sd = ifelse(tot_sampling_events > 50,0.00665, diff_sd))
-
-
-jags.data = list(Y = ebu_diff_temp$ch4_diff,
-                 tau.obs = 1/((ebu_diff_temp$diff_sd)) ^ 2,
-                 N = nrow(ebu_diff_temp),
-                 temp = as.numeric(ebu_diff_temp$temp_for_model_C))
-
-nchain = 3
-chain_seeds <- c(200,800,1400)
-init <- list()
-for(i in 1:nchain){
-  init[[i]] <- list(sd.pro = runif(1, 1, 3),
-                    A = runif(1, 0.24,1),
-                    a = runif(1, 0.023,1),
-                    .RNG.name = "base::Wichmann-Hill",
-                    .RNG.seed = chain_seeds[i])
+arrhenius_diff_posterior_prediction <- function(A, a, temp, Q){
+  est = (A*exp(a*(temp))) + rnorm(1000, 0, sd = Q)
+  return(est)
 }
 
-j.model   <- jags.model(file = ahrennius_diff_model,
-                        data = jags.data,
-                        inits = init,
-                        n.chains = 3)
+validate_models <- list()
+validation <- list()
 
-eval_diff  <- coda.samples(model = j.model,
-                          variable.names = c("sd.pro","A","a"),
-                          n.iter = 20000, n.burnin = 2000, thin = 20)
-plot(eval_diff)
-
-lake_coefficients <- eval_diff %>%
-  spread_draws(sd.pro, A, a) %>%
-  filter(.chain == 1)
-
-lake_coefficients_DF <- lake_coefficients %>%
-  summarise(mean_E20 = mean(A),
-            sd_E20 = sd(A),
-            upper_95_E20 = quantile(A, 0.95, na.rm = T),
-            lower_95_E20 = quantile(A, 0.05, na.rm = T),
-            mean_omega = mean(a),
-            sd_omega = sd(a),
-            upper_95_omega = quantile(a, 0.95, na.rm = T),
-            lower_95_omega = quantile(a, 0.05, na.rm = T),
-            mean_process = mean(sd.pro),
-            sd_process = sd(sd.pro),
-            upper_95_process = quantile(sd.pro, 0.95, na.rm = T),
-            lower_95_process = quantile(sd.pro, 0.05, na.rm = T))
-
-
-
-### MONTHLY TIMESTEP JUST RESERVOIRS ###
-ebu_diff_temp <- base %>% select(waterbody_type, lat, lon, ch4_diff, temp_for_model_K, waterbody_id, year, month, tot_sampling_events) %>%
-  na.omit(.) %>%
-  filter(waterbody_type == "reservoir") %>%
-  mutate(temp_for_model_C = temp_for_model_K-273.15) %>%
-  mutate(diff_sd = ifelse(tot_sampling_events < 5,93.6, NA),
-         diff_sd = ifelse(tot_sampling_events >= 5,25.3, diff_sd),
-         diff_sd = ifelse(tot_sampling_events > 10,2.83, diff_sd),
-         diff_sd = ifelse(tot_sampling_events > 15,6.78, diff_sd),
-         diff_sd = ifelse(tot_sampling_events > 20,0.819, diff_sd),
-         diff_sd = ifelse(tot_sampling_events > 25,0.902, diff_sd),
-         diff_sd = ifelse(tot_sampling_events > 30,0.973, diff_sd),
-         diff_sd = ifelse(tot_sampling_events > 40,0.0661, diff_sd),
-         diff_sd = ifelse(tot_sampling_events > 50,0.00665, diff_sd))
-
-
-jags.data = list(Y = ebu_diff_temp$ch4_diff,
-                 tau.obs = 1/((ebu_diff_temp$diff_sd)) ^ 2,
-                 N = nrow(ebu_diff_temp),
-                 temp = as.numeric(ebu_diff_temp$temp_for_model_C))
-
-nchain = 3
-chain_seeds <- c(200,800,1400)
-init <- list()
-for(i in 1:nchain){
-  init[[i]] <- list(sd.pro = runif(1, 1, 3),
-                    A = runif(1, 0.24,1),
-                    a = runif(1, 0.023,1),
-                    .RNG.name = "base::Wichmann-Hill",
-                    .RNG.seed = chain_seeds[i])
+for(h in 1:length(lake_type)){
+  
+  parms <- out_raw_coefficients %>%
+    filter(waterbody_type == lake_type[h]) %>%
+    sample_n(., 1000, replace=TRUE)
+  
+  temp_data <- error_diff %>% 
+    filter(waterbody_type == lake_type[h]) %>%
+    select(temp_for_model_C)
+  
+  temp_data <- c(temp_data$temp_for_model_C)
+  
+  diff_data <- error_diff %>% 
+    filter(waterbody_type == lake_type[h]) %>%
+    select(ch4_diff, temp_for_model_C, waterbody_type)
+  
+  predict_output <- list()
+  
+  for(s in 1:length(temp_data)){
+    
+    prediction <- arrhenius_diff_posterior_prediction(temp = temp_data[s],
+                                                     A = parms$A,
+                                                     a = parms$a,
+                                                     Q = parms$sd.pro)
+    predict_output[[s]] <- prediction
+  }
+  
+  prediction_output = as.data.frame(do.call(rbind, predict_output))
+  
+  prediction_output_long <- prediction_output %>% t(.) %>% reshape2::melt(.) %>%
+    group_by(Var2) %>%
+    summarize(mean = mean(value),
+              sd = sd(value),
+              var = var(value))
+  
+  validate <- cbind(prediction_output_long, diff_data)
+  
+  validation[[h]] <- validate
+  
+  plot(validate$temp_for_model_C, validate$ch4_diff, col = "blue4", pch = 16)
+  points(validate$temp_for_model_C, validate$mean, col = "black", pch = 21, bg = "red")
+  
+  NSE_RMSE <- cbind(prediction_output_long, diff_data) %>%
+    summarise(NSE = NSE(mean, ch4_diff),
+              rmse = rmse(mean, ch4_diff))
+  
+  validate_models[[h]] <- NSE_RMSE
+  
 }
 
-j.model   <- jags.model(file = ahrennius_diff_model,
-                        data = jags.data,
-                        inits = init,
-                        n.chains = 3)
-
-eval_diff  <- coda.samples(model = j.model,
-                           variable.names = c("sd.pro","A","a"),
-                           n.iter = 200000, n.burnin = 20000, thin = 200)
-plot(eval_diff)
-
-res_coefficients <- eval_diff %>%
-  spread_draws(sd.pro, A, a) %>%
-  filter(.chain == 1)
-
-res_coefficients_DF <- res_coefficients %>%
-  summarise(mean_E20 = mean(A),
-            sd_E20 = sd(A),
-            upper_95_E20 = quantile(A, 0.95, na.rm = T),
-            lower_95_E20 = quantile(A, 0.05, na.rm = T),
-            mean_omega = mean(a),
-            sd_omega = sd(a),
-            upper_95_omega = quantile(a, 0.95, na.rm = T),
-            lower_95_omega = quantile(a, 0.05, na.rm = T),
-            mean_process = mean(sd.pro),
-            sd_process = sd(sd.pro),
-            upper_95_process = quantile(sd.pro, 0.95, na.rm = T),
-            lower_95_process = quantile(sd.pro, 0.05, na.rm = T))
+validate_models = as.data.frame(do.call(rbind, validate_models))
+validation = as.data.frame(do.call(rbind, validation))
