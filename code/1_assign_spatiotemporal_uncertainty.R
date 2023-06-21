6575027596ec740a5f1df26b5f815ef5cdafb796
Ryan McClure <rmcclure@CIS-RMcclure-Air.local>
2023-06-21 09:06:20 -0600

    update according to MEL ideas

--- code/1_assign_spatiotemporal_uncertainty.R
+++ code/1_assign_spatiotemporal_uncertainty.R
@@ -18,23 +18,25 @@ pacman::p_load(tidyverse, MCMCvis, lubridate, tidybayes, readr,
 
 
 # read in data set that has been linked to GLCP Hydrobasin climate data
-base <- read_csv("./source_data/GLEE_data_with_GLCP_link.csv")
+base <- read_csv("./source_data/GLEE_data_with_GLCP_HWSD_link.csv")
 
 
 filtered_lakes <- base %>%
   ## --> quantify the number of total samples from each site by multiplying the
   ## --> number of months sampled by the number of sites collecting CH4 emissions
   mutate(tot_sampling_events = num_months_sampled * num_sites_sampled) %>%
-  group_by(lat, lon, waterbody_type) %>%
+  group_by(geometry, waterbody_type) %>%
   ## --> calculate a waterbody_id column for each individual lakes
   ## --> this will convey observation uncertainty by individual lakes
   mutate(waterbody_id = cur_group_id()) %>%
   ## --> remove any lakes that have only one measurement
-  filter(any(length(waterbody_id)>1)) %>%
+  #filter(any(length(waterbody_id)>1)) %>%
   ungroup() %>%
   ## --> assign the temperature used for the model to find either the observed air temp 
   ## --> or the air temperature as determined by the GCLP hydrobasin
-  mutate(temp_for_model_K = ifelse(is.na(mean_temp_k), effective_obs_wtemp_k, mean_temp_k))
+  mutate(temp_for_model_K = ifelse(is.na(mean_temp_k), effective_obs_wtemp_k, mean_temp_k)) %>%
+  group_by(waterbody_id) %>%
+  mutate(temp_for_model_K = ifelse(n_distinct(effective_obs_wtemp_k) == 0, mean_temp_k, effective_obs_wtemp_k))
 
 # Write the output of this lake thinning to a new file
 write_csv(filtered_lakes, "./output/filtered_GLEE_w_GLCP_link.csv")
@@ -42,17 +44,17 @@ write_csv(filtered_lakes, "./output/filtered_GLEE_w_GLCP_link.csv")
 # Calculate the standard deviation of the ebullition observations of each lake 
 error_ebu <- filtered_lakes %>% select(waterbody_id, waterbody_type,num_months_sampled, 
                                        num_sites_sampled, tot_sampling_events, ch4_ebu,
-                                       temp_for_model_K) %>% 
+                                       temp_for_model_K, surf_area_k, T_OC) %>% 
     ## --> filter only the ebullition observations
-    na.omit(.) %>%
     ## --> Group by waterbody ID
     group_by(waterbody_id) %>%
     ## --> calculate the SD based on the number of total months sampled, the number
     ## --> of sites sampled, and the product of sites and months sampled
     mutate(sd_time = (sqrt(sum(abs(ch4_ebu - mean(ch4_ebu))^2))/num_months_sampled),
            sd_space = (sqrt(sum(abs(ch4_ebu - mean(ch4_ebu))^2))/num_sites_sampled),
-           sd_space_time = (sqrt(sum(abs(ch4_ebu - mean(ch4_ebu))^2))/tot_sampling_events),
-           temp_for_model_C = temp_for_model_K - 273.15)
+           temp_for_model_C = temp_for_model_K - 273.15,
+           sd_time = ifelse(sd_time == 0, 2000, sd_time),
+           sd_space = ifelse(sd_space == 0, 1500, sd_space))
 
 # Write it to a file so you don't always have to rerun this script if R Fails
 write_csv(error_ebu, "./output/filtered_GLEE_ebullition_w_GLCP_link_and_ERROR.csv")
@@ -60,9 +62,8 @@ write_csv(error_ebu, "./output/filtered_GLEE_ebullition_w_GLCP_link_and_ERROR.cs
 # Calculate the standard deviation of the ebullition observations of each lake 
 error_diff <- filtered_lakes %>% select(waterbody_id, waterbody_type, num_months_sampled, 
                                         num_sites_sampled, tot_sampling_events, ch4_diff,
-                                        temp_for_model_K) %>% 
+                                        temp_for_model_K, surf_area_k, T_OC) %>% 
   ## --> filter only the diffusion observations
-  na.omit(.) %>%
   ## --> Group by waterbody ID
   group_by(waterbody_id) %>%
   ## --> calculate the SD based on the number of total months sampled, the number
@@ -70,18 +71,22 @@ error_diff <- filtered_lakes %>% select(waterbody_id, waterbody_type, num_months
   mutate(sd_time = (sqrt(sum(abs(ch4_diff - mean(ch4_diff))^2))/num_months_sampled),
          sd_space = (sqrt(sum(abs(ch4_diff - mean(ch4_diff))^2))/num_sites_sampled),
          sd_space_time = (sqrt(sum(abs(ch4_diff - mean(ch4_diff))^2))/tot_sampling_events),
-         temp_for_model_C = temp_for_model_K - 273.15)
+         temp_for_model_C = temp_for_model_K - 273.15,
+         sd_time = ifelse(sd_time == 0, 300, sd_time),
+         sd_space = ifelse(sd_space == 0, 400, sd_space))
 
 # Write it to a file so you don't always have to rerun this script if R Fails
 write_csv(error_diff, "./output/filtered_GLEE_diffusion_w_GLCP_link_and_ERROR.csv")
 
 # Make a global plot of the site locations used in the analysis
 
+base2 <- read_csv("./source_data/GLEE_data_with_GLCP_link.csv")
+
 ## --> Download the globe from R Natural Earth
 world <- ne_countries(scale = "medium", returnclass = "sf")
 
 ## --> Assign the flux pathway to the lakes and reservoirs
-k <- filtered_lakes %>%
+k <- base2 %>%
   select(lat, lon, waterbody_type, ch4_ebu, ch4_diff) %>%
   group_by(lat, lon, waterbody_type) %>%
   summarize_all(funs(mean)) %>%
